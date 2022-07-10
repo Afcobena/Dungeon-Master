fe84b85d2abfa3fb9b93361d80c7eb6da6c2e8d4
Adrian F. Cobena <adrianfcobena@gmail.com>
2022-07-10 02:25:11 +0200

    Objetivo 2 casi completado

--- js/champion.js
+++ js/champion.js
@@ -1,48 +1,48 @@
 class Pollito {
-    constructor() {
-      // aqui todas las propiedades del pollito
-      this.image = new Image()
-      this.image.src = "./src/images/champion.png"
-      this.x = 390; // posición eje X
-      this.y = 350; // posición eje Y
-      this.w = 70; // width: ancho
-      this.h = 100; // height: alto
-      this.speed = 10;
-      this.jumpSpeed = 40;
+  constructor() {
+    // aqui todas las propiedades del pollito
+    this.image = new Image()
+    this.image.src = "./src/images/champion.png"
+    this.x = 390; // posición eje X
+    this.y = 350; // posición eje Y
+    this.w = 70; // width: ancho
+    this.h = 100; // height: alto
+    this.speed = 10;
+    this.jumpSpeed = 40;
 
-    }
-  
-    // aqui todos los metodos del pollito
-  
-    drawPollito = () => {
-      ctx.drawImage(this.image, this.x, this.y, this.w, this.h)
-    }
-  
-    // gravedad del pollo
-    pollitoGravity = () => {
-      this.y = this.y + this.speed
-    }
-
-    //-------------------------------------MOVIMIENTO EN 4 EJES----------------------------------
-    polliMoveDown = () => {
-        this.y += this.speed
-
-    }
-
-    polliMoveForward = () => {
-        this.y -= this.speed
-
-    }
-
-    polliMoveLeft = () => {
-        this.x -= this.speed
-
-    }
-
-    polliMoveRight = () => {
-        this.x += this.speed
-
-    }
-  
-  
   }
+
+  // aqui todos los metodos del pollito
+
+  drawPollito = () => {
+    ctx.drawImage(this.image, this.x, this.y, this.w, this.h)
+  }
+
+  // gravedad del pollo
+  pollitoGravity = () => {
+    this.y = this.y + this.speed
+  }
+
+  //-------------------------------------MOVIMIENTO EN 4 EJES----------------------------------
+  polliMoveDown = () => {
+      this.y += this.speed
+
+  }
+
+  polliMoveForward = () => {
+      this.y -= this.speed
+
+  }
+
+  polliMoveLeft = () => {
+      this.x -= this.speed
+
+  }
+
+  polliMoveRight = () => {
+      this.x += this.speed
+
+  }
+
+
+}
